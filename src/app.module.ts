ef5e1146f3eb07d39eb10f20edbee52e0a8829b1
naushad0625 <bsse0625@iit.du.ac.bd>
2024-08-25 16:17:55 +0600

    Admin pannel added with admin home and products page.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,22 @@ import { ProductsController } from './products/products.controller';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProductsService } from './products/products.service';
+import { AdminController } from './admin/admin.controller';
+import { AdminModule } from './admin/admin.module';
+import { AdminProductsController } from './admin.products/admin.products.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
-  controllers: [AppController, ProductsController],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    AdminModule,
+  ],
+  controllers: [
+    AppController,
+    ProductsController,
+    AdminController,
+    AdminProductsController,
+  ],
   providers: [AppService, ProductsService],
 })
 export class AppModule {}
