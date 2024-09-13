1a0098fcc9c9c344cfd86ae281cff28dfc21b16e
naushad0625 <bsse0625@iit.du.ac.bd>
2024-09-13 21:36:31 +0600

    Global Validation pipe added, HttpExceptionFilter updated and Password validator decorator added

--- src/utils/password.validator.ts
+++ src/utils/password.validator.ts
@@ -9,13 +9,16 @@ import {
 @ValidatorConstraint({ async: true })
 class IsPasswordValidConatraints implements ValidatorConstraintInterface {
   validate(password: string): boolean {
+    const testresult = this.test(password);
+
+    if (!testresult) return false;
     const {
       hasDigit,
       hasUpperCase,
       hasLowercase,
       hasSpecialChar,
       isValidLength,
-    } = this.test(password);
+    } = testresult;
     return (
       hasDigit &&
       hasUpperCase &&
@@ -27,27 +30,41 @@ class IsPasswordValidConatraints implements ValidatorConstraintInterface {
 
   defaultMessage(args?: ValidationArguments): string {
     const { value } = args;
-    const {
-      hasDigit,
-      hasUpperCase,
-      hasLowercase,
-      hasSpecialChar,
-      isValidLength,
-    } = this.test(value);
+    const testresult = this.test(value);
+    if (!testresult) {
+      return 'Password must not be empty.';
+    } else {
+      const {
+        hasDigit,
+        hasUpperCase,
+        hasLowercase,
+        hasSpecialChar,
+        isValidLength,
+      } = testresult;
 
-    let message = 'Password must: ';
-    if (!hasDigit) message += ' contain at least 1 digit,';
-    if (!hasUpperCase) message += ' contain at least 1 uppercase letter,';
-    if (!hasLowercase) message += ' contain at least 1 lowercase letter,';
-    if (!hasSpecialChar) message += ' cntain at least 1 special character,';
-    if (!isValidLength)
-      message += ' contain minimum 8 characters and maximum 15 characters';
+      let message = 'Password must: ';
+      if (!hasDigit) message += ' contain at least 1 digit,';
+      if (!hasUpperCase) message += ' contain at least 1 uppercase letter,';
+      if (!hasLowercase) message += ' contain at least 1 lowercase letter,';
+      if (!hasSpecialChar) message += ' cntain at least 1 special character,';
+      if (!isValidLength)
+        message += ' contain minimum 8 characters and maximum 15 characters';
 
-    message = message.replace(/,\s*$/, '.') || 'Password is invalid.';
-    return message;
+      message = message.replace(/,\s*$/, '.') || 'Password is invalid.';
+      return message;
+    }
   }
 
-  test(password: string) {
+  test(password: string):
+    | false
+    | {
+        hasDigit: boolean;
+        hasUpperCase: boolean;
+        hasLowercase: boolean;
+        hasSpecialChar: boolean;
+        isValidLength: boolean;
+      } {
+    if (!password) return false;
     const hasDigit = /\d/.test(password);
     const hasUpperCase = /[A-Z]/.test(password);
     const hasLowercase = /[a-z]/.test(password);
