18e5de584c42ff9dfdcff1b85e146c6db46f00fd
naushad0625 <bsse0625@iit.du.ac.bd>
2024-09-14 22:09:49 +0600

    Few errors are fixed.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductsController } from './products/products.controller';
-import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
-import { ProductsService } from './products/products.service';
+import { AdminProductsController } from './admin.products/admin.products.controller';
 import { AdminController } from './admin/admin.controller';
 import { AdminModule } from './admin/admin.module';
-import { AdminProductsController } from './admin.products/admin.products.controller';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { ProductsController } from './products/products.controller';
+import { ProductsService } from './products/products.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     AdminModule,
+    AuthModule,
   ],
   controllers: [
     AppController,
